586e4d3ee8333d35b19ba05d545db78db894b3e8
Helana <helanan@gmail.com>
2017-01-23 20:42:36 -0600

    added age excercises and sonnet

---
+++ age.js
@@ -0,0 +1,47 @@
+// 1. How many hours are in a year
+// 2. How many minutes are in a decade
+// 3. How many seconds old they are
+// 4. If they are older than some arbitrary number, console "I'm old", else "I'm young"
+
+var hoursInYear = 365 * 24;
+console.log("Hours in Year: ", hoursInYear)
+
+var minutesPerHour = 60;
+var hoursPerDay = 24;
+var daysPerYear = 365;
+var yearsPerDecade = 10;
+console.log("minutes in decade: ", minutesPerHour * hoursPerDay * daysPerYear * yearsPerDecade);
+
+age1 = 7;
+age2 = 8;
+age3 = 9;
+age4 = 27;
+
+if (age1) {
+
+	document.getElementById('age1').innerHTML = age1 * year;
+} else
+
+if (age2) {
+	document.getElementById('age2').innerHTML = age2 * year;
+} else
+if (age3) {
+	document.getElementById('age3').innerHTML = age3 * year;
+} else
+if (age4) {
+	document.getElementById('age4').innerHTML = age4 * year;
+}
+
+
+
+
+
+var yearText = hoursInYear + " "+ "hours" + " ";
+document.getElementById('hours').innerHTML = yearText;
+
+var decadeText = 525,600 + " " + "minutes" + " ";
+document.getElementById('minutes').innerHTML = decadeText;
+
+if () {
+	document.getElementById('old').innerHTML = decade; 
+};
