74ee2e63d052bbe7b6d8992e3c3bf231850b587d
Helana <helanan@gmail.com>
2017-01-24 21:43:05 -0600

    added the rest of the array for loops

--- functions.js
+++ functions.js
@@ -49,4 +49,37 @@ animals.splice(1, 1);
 var colors = ["red", "blue", "gold", "pink", "orange"];
 var finalAnswer = colors.sort().reverse().join(" cat ");
 
+// Looping through arrays with FOR LOOPS
+for (var i=0; i<5; i++){
+	console.log("i", i);
+}
+
+// Simple example of variable hoisting
+var fruit = ["cherries", "apples", "bananas"];
+for(var j=0; j<fruit.length; j++){
+	console.log("fruit", fruit[j]);
+}
+
+// An example of a for loop that increments by 10 each time = 10 times 10, 20, 30, 50...100
+for (var x=0; x<=100; x+=10) {
+	console.log("x", x);
+}
+
+//for loop that pushes the index to an array for 5 times// console.log array [0,1,2,3,]
+numbers = [0,1,2,3,4,5]
+for (i = 0; i < cars.length; i++) { 
+    text += cars[i] + "<br>";
+}
+
+//for loop that starts at 100 and divides by 2 each time 3 times = 
+for (var i=100; i<4; i/2) {
+	console.log("p", p);
+}
+
+// //an example of an array map function
+// var ar = [1, 2, 3, 4, 5];
+// var ar2 = ar.map( function(v) { return v*1; } );
+// console.log( ar2 );
+
+
 
